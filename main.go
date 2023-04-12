91821418fd8e86eb3e29c711866093194b350f05
robmonte <17119716+robmonte@users.noreply.github.com>
2023-04-12 07:42:37 -0500

    Add another env var

--- main.go
+++ main.go
@@ -7,14 +7,27 @@ import (
 )
 
 func main() {
-	secret, found := os.LookupEnv("GHAS_ACCEPTANCE_TEST_ONE")
+	envVar1 := "sinkerAWSSM_Test_Key_1_Bad_Symbols________"
+	secret, found := os.LookupEnv(envVar1)
 	if !found || secret == "" {
-		log.Fatalln("Failed to find GHAS_ACCEPTANCE_TEST_ONE value")
+		log.Fatalf("Failed to find %q value\n", envVar1)
 	}
 
 	if secret != "I am secret one!" {
-		log.Fatalln("Received GHAS_ACCEPTANCE_TEST_ONE value but did not match expected value")
+		log.Fatalf("Received %q value but did not match expected value\n", envVar1)
 	} else {
-		fmt.Printf("The value of GHAS_ACCEPTANCE_TEST_ONE matched the expected value! Yay!")
+		fmt.Printf("The value of %q matched the expected value! Yay!\n", envVar1)
+	}
+
+	envVar2 := "sinkerGHAS-Test-Secret-2"
+	secret, found = os.LookupEnv(envVar2)
+	if !found || secret == "" {
+		log.Fatalf("Failed to find %q value\n", envVar2)
+	}
+
+	if secret != "I am secret one!" {
+		log.Fatalf("Received %q value but did not match expected value\n", envVar2)
+	} else {
+		fmt.Printf("The value of %q matched the expected value! Yay!\n", envVar2)
 	}
 }
