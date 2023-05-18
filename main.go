91faf47834530a8332b90072be1273caacdc9b01
robmonte <17119716+robmonte@users.noreply.github.com>
2023-05-18 16:14:19 -0500

    move uuid

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
 
-// var uuid string
+var uuid string
+
 func main() {
 	testType, exists := os.LookupEnv("TYPE")
 	if !exists {
 		log.Fatalf("missing required \"TYPE\" field to determine test type")
 	}
 
-	// uuid, exists = os.LookupEnv("UUID")
-	// if !exists {
-	// 	log.Fatalf("missing mirequired \"UUID\" field to find test values")
-	// }
+	uuid, exists = os.LookupEnv("UUID")
+	if !exists {
+		log.Fatalf("missing mirequired \"UUID\" field to find test values")
+	}
 
 	log.Printf("Running %q test type\n", testType)
 
@@ -113,6 +115,7 @@ func loadTypeTokenTestCheck() {
 func loadTypeAppTestCheck() {
 	// App Load Tets 1
 	envVar5 := "STOREGHAS_TEST_APP_LOAD_KEY_1"
+	fmt.Printf("%v\n", []byte(uuid))
 	secret, found := os.LookupEnv(envVar5)
 	if !found || secret == "" {
 		log.Fatalf("failed to find %q value\n", envVar5)
