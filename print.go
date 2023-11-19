c890877353a90440f6c064b8e919b66b13e19185
robmonte <17119716+robmonte@users.noreply.github.com>
2023-11-18 20:29:34 -0600

    Update print.go

--- print.go
+++ print.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/robmonte/private-printer-restricted/privateprinterrestricted"
 	"github.com/robmonte/private-printer/privateprinter"
 )
 
+const INCREMENT_ME int = 1
+
 func Print() {
-	privateprinter.PrivatePrint("dependabot test with access 1")
-	privateprinterrestricted.PrivatePrint("dependabot test without access 1")
+	privateprinter.PrivatePrint(fmt.Sprintf("dependabot test with access %d", INCREMENT_ME))
+	privateprinterrestricted.PrivatePrint(fmt.Sprintf("dependabot test no access %d", INCREMENT_ME))
 }
